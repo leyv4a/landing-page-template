92d950b97b57df4e7103730b08642388bc9f33b6
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-05-03 13:52:58 -0700

    feat(context) add ThemeProvider context to handle theme switching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import NavigationBar from '../sections/NavigationBar'
 
 export default function Home() {
   return (
-    <div>Home</div>
+  <>
+    <NavigationBar/>
+  </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,57 @@
+
+const { nextui } = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui({
+    prefix: "nextui", // prefix for themes variables
+    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+    defaultTheme: "light", // default theme from the themes object
+    defaultExtendTheme: "light", // default theme to extend on custom themes
+    layout: {}, // common layout tokens (applied to all themes)
+    themes: {
+      light: {
+        colors: {
+          background: "#FAFAFA", 
+          foreground: 
+           {
+            50: "#000000",
+            100: "#212121",
+            200: "#424242",
+            300: "#616161",
+            400: "#757575",
+            500: "#9E9E9E",
+            600: "#BDBDBD",
+            700: "#E0E0E0",
+            800: "#EEEEEE",
+            900: "#F5F5F5"
+          }
+        }, // light theme colors
+      },
+      dark: {
+        colors: {
+          background: "#18181B", 
+          foreground: {
+            50: "#F5F5F5",
+            100: "#EEEEEE",
+            200: "#E0E0E0",
+            300: "#BDBDBD",
+            400: "#9E9E9E",
+            500: "#757575",
+            600: "#616161",
+            700: "#424242",
+            800: "#212121",
+            900: "#000000"
+          }
+        }, // dark theme colors
+      },
+      // ... custom themes
+    },
+  })],
 }
 
